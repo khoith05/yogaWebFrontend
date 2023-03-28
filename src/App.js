088dfb4f305f205931c7cd6fbfa566d062397cd9
DESKTOP-RBBN240\admin <navt2010@gmail.com>
2023-03-28 11:50:07 +0700

    add page Exercise detail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './views/Banner';
 import Layout from './views/Layout';
 import Camera from './components/Camera';
 import { Route,Routes } from 'react-router-dom';
+import ExercisePage from './views/ExercisePage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,6 +16,7 @@ function App() {
           {/* <Route path="dashboard" element={<Dashboard />} />
 
           <Route path="*" element={<NoMatch />} /> */}
+          <Route path="/exercise" element={<ExercisePage/>} />
         </Route>
       </Routes>
       
---
+++ src/views/ExercisePage.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {useTheme} from '@table-library/react-table-library/theme'
+import {Container, Row} from 'react-bootstrap'
+import ExerciseDetail from './ExerciseDetail';
+import {HashLink} from 'react-router-hash-link';
+import './ExercisePage.css'
+
+const ExercisePage= () => {
+
+
+  return (
+    <div className="banner1" >
+    <Container>
+      <Row>
+      <h10>
+      Restore & Rejuvenate
+      </h10>
+      <h9>30 mins
+        <HashLink to = ''>
+      <button className="my-button"><span>Instructions for placing the camera</span></button></HashLink></h9> 
+      
+      </Row>
+    </Container>
+    <Container>
+    <ExerciseDetail></ExerciseDetail>
+    </Container>
+    <Container>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <button className="my-button1">Start</button>
+    </div>
+    </Container>
+    
+    </div>
+  );
+        
+
+}
+
+export default ExercisePage;
