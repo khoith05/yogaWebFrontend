2698d92cd467817c12216e9d0985f5ee90d5f2fc
TRAN HOANG KHOI <khoith@dgroup.co>
2023-05-07 22:38:40 +0700

    fix video stop

--- src/views/YogaVideo.js
+++ src/views/YogaVideo.js
@@ -1,46 +1,30 @@
 import { memo } from "react";
-import { Transition } from "react-transition-group";
-import { useRef } from "react";
 
 function YogaVideo(props) {
   const { url, onFinish, size, showVideo } = props;
-  const nodeRef = useRef(null);
 
   const handleVideoEnd = () => {
     onFinish();
   };
 
   const transitionStyles = {
-    entering: {
-      width: size.width * 0.3,
-      height: size.height * 0.3,
-    },
-    entered: {
-      width: size.width * 0.3,
-      height: size.height * 0.3,
-    },
+    width: size.width * 0.3,
+    height: size.height * 0.3,
   };
 
+  const style = showVideo ? size : transitionStyles;
+
   return (
-    <Transition
-      nodeRef={nodeRef}
-      in={!showVideo}
-      timeout={{ appear: 0, enter: 500 }}
-      exit={false}
-    >
-      {(state) => (
-        <video
-          className="camera-element pose-image"
-          src={url}
-          style={{ ...size, ...transitionStyles[state] }}
-          autoPlay
-          muted
-          controls={false}
-          onEnded={handleVideoEnd}
-          preload="auto"
-        />
-      )}
-    </Transition>
+    <video
+      className="camera-element pose-image"
+      src={url}
+      style={style}
+      autoPlay
+      muted
+      controls={false}
+      onEnded={handleVideoEnd}
+      preload="auto"
+    />
   );
 }
 
