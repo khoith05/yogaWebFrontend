ae559fa0692cf2454aa43cacbec46af311c84842
Tran Hoang Khoi <t.hoangkhoi312@gmail.com>
2023-05-10 20:57:54 +0700

    style code

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,35 +1,40 @@
-function Login () {
-
-    return (
-      <div className="login">
-          <div className="wrapper">
-              <div className= "form-box"> 
-                  <h2>Login</h2>
-                  <form action="#">
-                      <div className="input-box">
-                          <span class="icon">
-                              <ion-icon name="mail"></ion-icon>
-                          </span>
-                          <input type="email" required />
-                          <label>Email</label>
-                      </div>
-                      <div class="input-box">
-                          <span class="icon">
-                              <ion-icon name="lock-closed"></ion-icon>
-                          </span>
-                          <input type="password" required />
-                          <label>Password</label>
-                      </div>
-                      <button type="submit" class="btn">Submit</button>
-                      <div class="login-register">
-                          <p>Don't have an account? <a href="signup" 
-                          class="register-link">Register</a></p>
-                      </div>
-                  </form>
-              </div>
-          </div>
+function Login() {
+  return (
+    <div className="login">
+      <div className="wrapper">
+        <div className="form-box">
+          <h2>Login</h2>
+          <form action="#">
+            <div className="input-box">
+              <span className="icon">
+                <ion-icon name="mail"></ion-icon>
+              </span>
+              <input type="email" required />
+              <label>Email</label>
+            </div>
+            <div className="input-box">
+              <span className="icon">
+                <ion-icon name="lock-closed"></ion-icon>
+              </span>
+              <input type="password" required />
+              <label>Password</label>
+            </div>
+            <button type="submit" className="btn">
+              Submit
+            </button>
+            <div className="login-register">
+              <p>
+                Don't have an account?{" "}
+                <a href="signup" className="register-link">
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    )
-  }
-  
-  export default Login;
+    </div>
+  );
+}
+
+export default Login;
