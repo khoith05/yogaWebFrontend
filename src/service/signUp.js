ded7132c0dd3a3a50d44bd97f01d5fdd1308f4b5
Tran Hoang Khoi <t.hoangkhoi312@gmail.com>
2023-05-11 00:55:57 +0700

    sign up

---
+++ src/service/signUp.js
@@ -0,0 +1,21 @@
+import Api from "../utils/api";
+import { SIGNUP_LOADING } from "../utils/constant";
+
+export default async function signUp({ username, email, password }) {
+  const { status, data } = await Api.post({
+    url: `/api/auth/signup`,
+    auth: false,
+    key: SIGNUP_LOADING,
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  if (status !== 200) {
+    return { message: String(data.message) };
+  }
+
+  return data.data;
+}
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,47 +1,124 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { SIGNUP_LOADING } from "../utils/constant";
+import signUp from "../service/signUp";
+import { selectIsLoading } from "../store/loading";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../store/user";
+
 function Signup() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+    confirmPassword: "",
+  });
+
+  const isLoading = useSelector((state) =>
+    selectIsLoading(state, [SIGNUP_LOADING])
+  );
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      !formData.password ||
+      !formData.confirmPassword ||
+      formData.confirmPassword < 6 ||
+      formData.password < 6
+    ) {
+      return setError("Password is too short");
+    }
+    if (formData.password !== formData.confirmPassword) {
+      return setError("Password not match");
+    }
+    if (!formData.username || formData.username.length < 6) {
+      return setError("Username is too short");
+    }
+
+    signUp(formData).then(({ message, username, email }) => {
+      setError(message || "");
+      if (username && email) {
+        dispatch(setUser({ username, email }));
+        navigate("/");
+      }
+    });
+  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
   return (
     <div className="signup">
       <div className="wrapper">
         <div className="form-box">
           <h2>Registration</h2>
-          <form action="#">
+          <form onSubmit={handleSubmit}>
             <div className="input-box">
               <span className="icon">
                 <ion-icon name="mail"></ion-icon>
               </span>
-              <input type="username" required />
+              <input
+                type="email"
+                name="email"
+                required
+                value={formData.email}
+                onChange={handleInputChange}
+              />
               <label>Email</label>
             </div>
             <div className="input-box">
               <span className="icon">
                 <ion-icon name="person"></ion-icon>
               </span>
-              <input type="username" required />
+              <input
+                type="username"
+                name="username"
+                required
+                value={formData.username}
+                onChange={handleInputChange}
+              />
               <label>Username</label>
             </div>
             <div className="input-box">
               <span className="icon">
                 <ion-icon name="lock-closed"></ion-icon>
               </span>
-              <input type="password" required />
+              <input
+                type="password"
+                name="password"
+                required
+                value={formData.password}
+                onChange={handleInputChange}
+              />
               <label>Password</label>
             </div>
             <div className="input-box">
               <span className="icon">
                 <ion-icon name="lock-closed"></ion-icon>
               </span>
-              <input type="password" required />
+              <input
+                type="password"
+                name="confirmPassword"
+                required
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+              />
               <label>Re-enter Password</label>
             </div>
-            <button type="submit" className="btn">
+            <p className="text-danger">{error}</p>
+            <button type="submit" className="btn" disabled={isLoading}>
               Submit
             </button>
             <div className="login-register">
               <p>
                 Already have an account?{" "}
-                <a href="login" className="register-link">
+                <Link to="/login" className="register-link">
                   Login
-                </a>
+                </Link>
               </p>
             </div>
           </form>
